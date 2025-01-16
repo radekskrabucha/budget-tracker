53d48239d0a550cd8ce7355433cbd00bc65a2c2a
radekskrabucha <rskrabucha13@gmail.com>
2025-01-16 15:06:14 +0100

    feat(api): added auth handler with cors middleware

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,8 @@
 import { getRandomInt } from '@budget-tracker/utils'
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { auth } from '~/api/lib/auth'
 import { notFound } from '~/api/middleware/notFound'
 import { onError } from '~/api/middleware/onError'
 import { pinoLogger } from '~/api/middleware/pinoLogger'
@@ -10,6 +12,13 @@ import { env } from '~/api/utils/env'
 
 export const app = new Hono<AppBindings>()
   .use(pinoLogger())
+  .use(
+    '*',
+    cors({
+      origin: env.BETTER_AUTH_TRUSTED_ORIGINS,
+      credentials: true
+    })
+  )
   .get('/', c => {
     return c.text('Hello Hono!')
   })
@@ -17,7 +26,10 @@ export const app = new Hono<AppBindings>()
     return c.json({ randomNumber: getRandomInt(1, 10) })
   })
 
-app.use(serveEmojiFavicon('💸'))
+app.on(['POST', 'GET'], '/auth/*', c => auth.handler(c.req.raw))
+
+app.use('/favicon.ico', serveEmojiFavicon('💸'))
+
 app.notFound(notFound)
 app.onError(onError)
 
