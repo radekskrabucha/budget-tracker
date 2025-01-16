2804fd8371b27aaec820a09a06f6d54f31c56d80
radekskrabucha <rskrabucha13@gmail.com>
2025-01-16 11:18:25 +0100

    feat(api): added on error middleware

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,6 +2,7 @@ import { getRandomInt } from '@budget-tracker/utils'
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { notFound } from '~/api/middleware/notFound'
+import { onError } from '~/api/middleware/onError'
 import { serveEmojiFavicon } from '~/api/middleware/serveEmojiFavicon'
 import { env } from '~/api/utils/env'
 
@@ -15,6 +16,7 @@ export const app = new Hono()
 
 app.use(serveEmojiFavicon('💸'))
 app.notFound(notFound)
+app.onError(onError)
 
 const port = env.PORT
 console.log(`Server is running on http://localhost:${port}`)
