c5286c0c60c02ea3a301aab8eaab16549046083c
radekskrabucha <rskrabucha13@gmail.com>
2025-01-16 14:51:26 +0100

    feat(api): added auth db schema

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -1,6 +1,11 @@
 import { neon } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
 import { env } from '~/api/utils/env'
+import * as authSchema from './schema/auth.schema'
 
 const sql = neon(env.DATABASE_URL)
-export const db = drizzle(sql)
+export const db = drizzle(sql, {
+  schema: {
+    ...authSchema
+  }
+})
