f904e3e3975b06867d14311e4f1de811ae33fe06
radekskrabucha <rskrabucha13@gmail.com>
2025-01-17 10:35:36 +0100

    feat(api): integrate category and transaction schemas into the database configuration

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -2,10 +2,14 @@ import { neon } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
 import { env } from '~/api/utils/env'
 import * as authSchema from './schema/auth.schema'
+import * as categorySchema from './schema/category.schema'
+import * as transactionSchema from './schema/transaction.schema'
 
 const sql = neon(env.DATABASE_URL)
 export const db = drizzle(sql, {
   schema: {
-    ...authSchema
+    ...authSchema,
+    ...categorySchema,
+    ...transactionSchema
   }
 })
