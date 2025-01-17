299952dd209c1e07d95c67756e84bfece9476c4e
radekskrabucha <rskrabucha13@gmail.com>
2025-01-17 10:34:32 +0100

    feat(api): added transaction db schema

---
+++ apps/api/src/db/schema/transaction.schema.ts
@@ -0,0 +1,36 @@
+import {
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  numeric,
+  pgEnum
+} from 'drizzle-orm/pg-core'
+import { user } from './auth.schema'
+import { category } from './category.schema'
+
+export const transactionTypeEnum = pgEnum('transaction_type', [
+  'income',
+  'expense'
+])
+
+export const transaction = pgTable('transaction', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => user.id, {
+      onDelete: 'cascade'
+    }),
+  categoryId: uuid('category_id').references(() => category.id, {
+    onDelete: 'set null'
+  }),
+  amount: numeric('amount').notNull(),
+  description: text('description'),
+  date: timestamp('date').notNull(),
+  type: transactionTypeEnum('type').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date())
+})
