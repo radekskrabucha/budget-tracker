4c1adb46579151e814844f7743c5c1f5cdb724cb
radekskrabucha <rskrabucha13@gmail.com>
2025-01-24 15:13:31 -0500

    chore: format

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,8 +1,8 @@
 import { Hono } from 'hono'
 import type { AppBindings } from '../types/app'
+import { adminRouter as admin } from './admin'
 import { categoriesRouter } from './categories'
 import { testRouter } from './test'
-import { adminRouter as admin } from './admin'
 import { transactionsRouter } from './transactions'
 
 export const appRouter = new Hono<AppBindings>()
