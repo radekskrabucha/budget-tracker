dc56cb17b26afc355928dac4eac2f90f072b04ae
radekskrabucha <rskrabucha13@gmail.com>
2025-01-24 12:13:51 -0500

    feat(api): add categories client and update import paths for improved type safety

--- apps/api/src/lib/client.ts
+++ apps/api/src/lib/client.ts
@@ -1,5 +1,6 @@
 import { hc } from 'hono/client'
-import { app } from '../index'
+import { app } from '~/api/index'
+import type { categoriesRouter } from '~/api/routes/categories'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const client = hc<typeof app>('')
@@ -7,3 +8,11 @@ export type Client = typeof client
 
 export const hcWithType = (...args: Parameters<typeof hc>): Client =>
   hc<typeof app>(...args)
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const categoriesClient = hc<typeof categoriesRouter>('')
+export type CategoriesClient = typeof categoriesClient
+
+export const hcWithTypeCategories = (
+  ...args: Parameters<typeof hc<typeof categoriesRouter>>
+): CategoriesClient => hc<typeof categoriesRouter>(...args)
