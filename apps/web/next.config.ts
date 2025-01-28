2d3c798cc940f729ed3a300b3f627ac760175144
radekskrabucha <rskrabucha13@gmail.com>
2025-01-28 13:21:38 -0500

    chore(web): added react-compiler

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from 'next'
 import './src/utils/env/client'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    reactCompiler: true
+  }
 }
 
 export default nextConfig
