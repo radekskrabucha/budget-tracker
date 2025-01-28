68c3ad2583a28ac605deecc9084256fc6ade532d
radekskrabucha <rskrabucha13@gmail.com>
2025-01-28 16:13:54 -0500

    feat(web): create sign up page component

---
+++ apps/web/src/features/signUp/SignUpPage.tsx
@@ -0,0 +1,13 @@
+import { SignUpForm } from './components/SignUpForm'
+
+export const SignUpPage = () => (
+  <div className="layout-section flex-1 justify-center items-center gap-10">
+    <div className="flex flex-col gap-2 text-center">
+      <h1 className="text-2xl font-bold">Create an account</h1>
+      <p className="text-muted-foreground">
+        Enter your information to get started
+      </p>
+    </div>
+    <SignUpForm />
+  </div>
+)
