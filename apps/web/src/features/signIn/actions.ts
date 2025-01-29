9467b2d68e7b5f7aefdd3fc640b520f2c8926380
radekskrabucha <rskrabucha13@gmail.com>
2025-01-29 15:30:28 -0500

    chore: format

--- apps/web/src/features/signIn/actions.ts
+++ apps/web/src/features/signIn/actions.ts
@@ -2,12 +2,8 @@ import { authClient } from '~/web/lib/auth'
 
 const signInReq = authClient.signIn.email
 
-type SignInReq = Pick<
-  Parameters<typeof signInReq>[0],
-  'email' | 'password'
->
+type SignInReq = Pick<Parameters<typeof signInReq>[0], 'email' | 'password'>
 
 export const signIn = (data: SignInReq) => signInReq(data)
-
 
 export const signOut = authClient.signOut
