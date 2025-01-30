407a0e5ce84e11c81727c85ccfb7f9b42df6af5e
radekskrabucha <rskrabucha13@gmail.com>
2025-01-30 16:17:08 -0500

    feat(web): add transactions layout with add transaction button

---
+++ apps/web/src/features/transactions/TransactionsLayout.tsx
@@ -0,0 +1,27 @@
+import { buttonVariants } from '@budget-tracker/ui/components/ui/button'
+import { Plus } from 'lucide-react'
+import Link from 'next/link'
+import { InternalLink } from '~/web/config/app'
+
+export const TransactionsLayout = ({ children }: React.PropsWithChildren) => (
+  <>
+    <div className="layout-section">
+      <div className="flex items-center justify-between">
+        <Link
+          href={InternalLink.transactions}
+          className="text-2xl font-semibold"
+        >
+          Your transactions
+        </Link>
+        <Link
+          href={InternalLink.addTransaction}
+          className={buttonVariants()}
+        >
+          <Plus className="size-4" />
+          Add new transaction
+        </Link>
+      </div>
+    </div>
+    {children}
+  </>
+) 
