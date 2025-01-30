8c2cd242c4906c4850094ac3103998c97094ee3d
radekskrabucha <rskrabucha13@gmail.com>
2025-01-30 18:15:40 -0500

    feat(web): fetch and pass categories to AddTransactionForm

--- apps/web/src/features/transactions/AddTransactionPage.tsx
+++ apps/web/src/features/transactions/AddTransactionPage.tsx
@@ -1,13 +1,18 @@
+import { getUserCombinedCategories } from '~/web/features/categories/serverActions'
 import { AddTransactionForm } from './components/AddTransactionForm'
 
-export const AddTransactionPage = () => (
-  <>
-    <section className="layout-section">
-      <h2 className="text-2xl font-semibold">Add transaction</h2>
-      <p className="text-muted-foreground text-sm">
-        Add a new transaction to track your income or expenses.
-      </p>
-    </section>
-    <AddTransactionForm />
-  </>
-) 
+export const AddTransactionPage = async () => {
+  const data = await getUserCombinedCategories()
+
+  return (
+    <>
+      <section className="layout-section">
+        <h2 className="text-2xl font-semibold">Add transaction</h2>
+        <p className="text-muted-foreground text-sm">
+          Add a new transaction to track your income or expenses.
+        </p>
+      </section>
+      <AddTransactionForm categories={data.categories} />
+    </>
+  )
+}
