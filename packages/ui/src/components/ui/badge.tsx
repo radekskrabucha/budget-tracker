75646d13c99c00b1183ed7774e14d0ab3906e0e0
radekskrabucha <rskrabucha13@gmail.com>
2025-01-31 14:27:25 -0500

    refactor(ui): improve Badge component type definitions and export

--- packages/ui/src/components/ui/badge.tsx
+++ packages/ui/src/components/ui/badge.tsx
@@ -2,7 +2,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import * as React from 'react'
 import { cn } from '~/utils/styles.js'
 
-const badgeVariants = cva(
+export const badgeVariants = cva(
   'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
     variants: {
@@ -22,17 +22,16 @@ const badgeVariants = cva(
   }
 )
 
-export interface BadgeProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof badgeVariants> {}
+export type BadgeProps = VariantProps<typeof badgeVariants> &
+  React.ComponentPropsWithRef<'div'>
 
-function Badge({ className, variant, ...props }: BadgeProps) {
-  return (
-    <div
-      className={cn(badgeVariants({ variant }), className)}
-      {...props}
-    />
-  )
-}
-
-export { Badge, badgeVariants }
+export const Badge: React.FC<BadgeProps> = ({
+  className,
+  variant,
+  ...props
+}) => (
+  <div
+    className={cn(badgeVariants({ variant }), className)}
+    {...props}
+  />
+)
