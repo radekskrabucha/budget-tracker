5a327027afef4b896fe12dd62ad608b3b9827d96
radekskrabucha <rskrabucha13@gmail.com>
2025-01-31 16:20:44 -0500

    feat(web): add server action for fetching dashboard summary

---
+++ apps/web/src/features/home/serverActions.ts
@@ -0,0 +1,32 @@
+import {
+  fetchWrapper,
+  InferResponseType,
+  InferRequestType
+} from '@budget-tracker/api'
+import { appClient } from '~/web/lib/client'
+import { getHeaders } from '~/web/utils/headers'
+
+const getDashboardSummary = appClient.dashboard.summary.$get
+
+type GetDashboardSummaryReq = InferRequestType<
+  typeof getDashboardSummary
+>['query']
+
+export type GetDashboardSummaryRes = InferResponseType<
+  typeof getDashboardSummary
+>
+
+export const getUserDashboardSummary = async (req: GetDashboardSummaryReq) => {
+  const data = await fetchWrapper(
+    getDashboardSummary(
+      {
+        query: req
+      },
+      {
+        headers: await getHeaders()
+      }
+    )
+  )
+
+  return data
+}
