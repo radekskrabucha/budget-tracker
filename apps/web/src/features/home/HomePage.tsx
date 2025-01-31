b953e60ff4b8ee9efd813b312f7f194a5d5245a4
radekskrabucha <rskrabucha13@gmail.com>
2025-01-31 15:17:38 -0500

    feat(web): add RecentTransactions component to HomePage

--- apps/web/src/features/home/HomePage.tsx
+++ apps/web/src/features/home/HomePage.tsx
@@ -1,7 +1,9 @@
 import { HeaderSection } from './components/HeaderSection'
+import { RecentTransactions } from './components/RecentTransactions'
 
 export const HomePage = () => (
   <>
     <HeaderSection />
+    <RecentTransactions />
   </>
 )
