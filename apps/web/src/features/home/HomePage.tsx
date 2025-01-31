be24c64ad34d60c9cf07d6a5e641eb115639e0da
radekskrabucha <rskrabucha13@gmail.com>
2025-01-31 16:56:24 -0500

    feat(web): add SummarySection to HomePage

--- apps/web/src/features/home/HomePage.tsx
+++ apps/web/src/features/home/HomePage.tsx
@@ -1,9 +1,11 @@
 import { HeaderSection } from './components/HeaderSection'
 import { RecentTransactions } from './components/RecentTransactions'
+import { SummarySection } from './components/SummarySection'
 
 export const HomePage = () => (
   <>
     <HeaderSection />
+    <SummarySection />
     <RecentTransactions />
   </>
 )
